e8c9b32f4636489bc3bfceb57858de143e18ee1f
Cavid112233 <tu7s06nxd@code.edu.az>
2024-04-08 13:41:25 +0400

    initial commit

---
+++ ConsoleApp8/model/Group.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.method
+{
+    internal class Group; Student
+    {
+        public static Group[] Groups = new Group[0];
+        static int GroupCount {  get; set; }
+        public string GroupName { get; set; }
+        public int GroupId { get; set; }
+        Student[] Students =new Student[0];
+
+
+        public  void GetGroupInfo()
+        {
+            Console.WriteLine($"{ GroupId}{GroupName}");
+            foreach (Student students in Students)
+            {
+                student.GetInfo();
+            }
+        }
+        public void  GetStudent(int Id)
+        {
+            for( int i = 0; i<Students.Length; i++)
+            {
+                if(i == Id)
+                {
+                    Students[i].GetInfo();
+                }
+            }
+        } 
+          public void AddStudent(Student student)
+              {
+                  Array.Resize(ref Groups,Groups.Lenght +1)
+            {
+            Groups[Groups.Lenght - 1] = Group;
+            }
+              }
+              public void Search(string a)
+    {
+        foreach(Student student in Students)
+        {
+            if(a==student.Name|| a==student.Surname)
+            {
+                Console.WriteLine(student.GetInfo);
+            }
+
+        }
+    }
+    public override string ToString()
+    {
+        return $"{GroupID} {groupName}";
+        foreach (Student student in Students)
+        {
+            Console.WriteLine($"{student,Name} {student.Surname} {Student.Id}");
+        }
+    }
+    }
+}
---
+++ ConsoleApp8/model/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.method
+{
+    internal class Student
+    {
+        public static int Count  { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public Student (String Name,string Surname)
+        {
+            Name = Name;
+            Surname = Surname;
+            Count = Id;
+            Count++;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine( $"{Id} {Name} { Surname }"); 
+            
+        }
+
+    }
+}
